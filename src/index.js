7c5e30722e86b2b5b60c5f08b134fd79ab9f5cc5
Michael D'Avino <mdavino132@gmail.com>
2020-10-06 22:15:23 -0400

    Hopefully fixs

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
 ReactDOM.render(<App />, document.getElementById("root"));
+require('file-loader?name=[name].[ext]!../index.html');
